0f12c47c2e3ecfdcb920f6f05fbc4d3e0524048f
Yu-wen Pwu <ywpu@cs.nctu.edu.tw>
2016-12-01 20:44:19 +0800

    Add summary of slide 9 & 11.

--- select-poll/note.c
+++ select-poll/note.c
@@ -18,3 +18,39 @@ int fcntl(int fd, int cmd, ... /* arg */ );
  * commonly used file descriptor operations:
  * F_GETFL, F_SETFL, F_GETOWN, F_SETOWN
  */
+
+#include <sys/socket.h>
+#include <netdb.h>
+
+struct hostent *gethostbyname(const char *name);
+struct hostent *gethostbyname2(const char *name, int family);
+struct hostent *gethostbyaddr(const void *addr, socklen_t len, int family);
+
+int gethostbyname_r(const char *name,
+                    struct hostent *ret, char *buf, size_t buflen,
+                    struct hostent **result, int *h_errnop);
+int gethostbyname2_r(const char *name, int family,
+                     struct hostent *ret, char *buf, size_t buflen,
+                     struct hostent **result, int *h_errnop);
+int gethostbyaddr_r(const void *addr, socklen_t len, int family,
+                    struct hostent *ret, char *buf, size_t buflen,
+                    struct hostent **result, int *h_errnop);
+
+struct servent *getservbyname(const char *name, const char *proto);
+struct servent *getservbyport(int port, const char *proto);
+
+/**
+ * foo_r():
+ * reentrant version of foo()
+ */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+int getaddrinfo(const char *host, const char *serv,
+                const struct addrinfo *hints, struct addrinfo **res);
+void freeaddrinfo(struct addrinfo *res);
+int getnameinfo(const struct sockaddr *sa, socklen_t salen,
+                char *host, socklen_t hostlen,
+                char *serv, socklen_t servlen, int flags);
