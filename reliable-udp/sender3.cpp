48596cf37f3cad9fffaeef2ca6d0fbcd2550b6de
Yu-wen Pwu <ywpu@cs.nctu.edu.tw>
2016-12-06 03:11:00 +0800

    Finished sender but not tested yet.

--- reliable-udp/sender3.cpp
+++ reliable-udp/sender3.cpp
@@ -1,16 +1,19 @@
 #include <algorithm>
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <stdio.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/time.h>
 
 char buf[1024];
 char tmp[256];
 
-void sendWindow(int fd, unsigned baseSeqNo, const void *data, unsigned dataLen);
+void sendWindow(int fd, unsigned baseSeqNo, const char *data, unsigned dataLen);
+void finalize(int fd);
 
 int main(int argc, char **argv) {
 	if (argc != 4)
@@ -36,10 +39,25 @@ int main(int argc, char **argv) {
 	if (connect(fd, (const struct sockaddr *) &sa, sizeof(sa)) < 0)
 		perror("connect() error");
 
+	std::ifstream fs(argv[3], std::ios::in | std::ios::binary);
+	if (!fs.is_open())
+		fprintf(stderr, "cannot read %s\n", argv[3]);
+
+	unsigned no = 0;
+	char data[129024];
+	while (fs.good()) {
+		fs.read(data, 129024);
+		sendWindow(fd, no, data, (unsigned) fs.gcount());
+		no += 128;
+	}
+
+	fs.close();
+	finalize(fd);
+
 	return 0;
 }
 
-void sendWindow(int fd, unsigned baseSeqNo, const void *data, unsigned dataLen) {
+void sendWindow(int fd, unsigned baseSeqNo, const char *data, unsigned dataLen) {
 	std::vector<unsigned> remainingPackets(128);
 	for (unsigned i = 0; i < 128; ++i)
 		remainingPackets[i] = i;
@@ -52,8 +70,9 @@ void sendWindow(int fd, unsigned baseSeqNo, const void *data, unsigned dataLen) 
 
 			// LEN
 			//   length of data, in text, 8 bytes
-			unsigned len = dataLen - *i * 1008;
-			len = len < 0 ? 0 : len > 1008 ? 1008 : len;
+			int slen = dataLen - *i * 1008;
+			slen = slen < 0 ? 0 : slen > 1008 ? 1008 : slen;
+			unsigned len = (unsigned) slen;
 			sprintf(tmp, "%08d", len);
 			strcat(buf, tmp);
 
@@ -94,6 +113,31 @@ void sendWindow(int fd, unsigned baseSeqNo, const void *data, unsigned dataLen) 
 		}
 
 		if (done)
+			break;
+	}
+}
+
+void finalize(int fd) {
+	int count = 0;
+	while (count < 64) {
+		count++;
+
+		// FIN
+		//   tell receiver to close, in text, 8 bytes
+		strcpy(buf, "FFFFFFFF");
+		if (send(fd, buf, 8, 0) < 0)
+			perror("send() error");
+
+		if (recv(fd, buf, 8, 0) < 0) {
+			if (errno != EWOULDBLOCK)
+				perror("recv() error");
+			continue;
+		}
+
+		// FIN_ACK
+		//   receiver got it, in text, 8 bytes
+		strcpy(tmp, "EEEEEEEE");
+		if (!memcmp(buf, tmp, 8))
 			break;
 	}
 }
